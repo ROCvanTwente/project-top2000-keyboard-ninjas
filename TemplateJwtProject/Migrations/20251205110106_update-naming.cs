542dae085a9f79f979fb9d8f7f4c6ca616b90e77
Sem Christenhusz <1357767@student.rocvantwente.nl>
2025-12-10 09:24:10 +0100

    Add Top2000 models and migrations for songs and artists
    Introduced new EF Core models: Artist, Songs, Top2000Entry, with navigation properties and relationships. Updated AppDbContext and added migrations to create and rename tables and columns. Updated model snapshot and supporting files.

---
+++ TemplateJwtProject/Migrations/20251205110106_update-naming.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TemplateJwtProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatenaming : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Top2000Entry_Song_SongId",
+                table: "Top2000Entry");
+
+            migrationBuilder.DropTable(
+                name: "Song");
+
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    SongsId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ArtistId = table.Column<int>(type: "int", nullable: false),
+                    Titel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleaseYear = table.Column<int>(type: "int", nullable: true),
+                    ImgUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Lyrics = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Youtube = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.SongsId);
+                    table.ForeignKey(
+                        name: "FK_Songs_Artist_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artist",
+                        principalColumn: "ArtistId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_ArtistId",
+                table: "Songs",
+                column: "ArtistId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Top2000Entry_Songs_SongId",
+                table: "Top2000Entry",
+                column: "SongId",
+                principalTable: "Songs",
+                principalColumn: "SongsId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Top2000Entry_Songs_SongId",
+                table: "Top2000Entry");
+
+            migrationBuilder.DropTable(
+                name: "Songs");
+
+            migrationBuilder.CreateTable(
+                name: "Song",
+                columns: table => new
+                {
+                    SongId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ArtistId = table.Column<int>(type: "int", nullable: false),
+                    ImgUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Lyrics = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReleaseYear = table.Column<int>(type: "int", nullable: true),
+                    Titel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Youtube = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Song", x => x.SongId);
+                    table.ForeignKey(
+                        name: "FK_Song_Artist_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artist",
+                        principalColumn: "ArtistId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Song_ArtistId",
+                table: "Song",
+                column: "ArtistId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Top2000Entry_Song_SongId",
+                table: "Top2000Entry",
+                column: "SongId",
+                principalTable: "Song",
+                principalColumn: "SongId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
