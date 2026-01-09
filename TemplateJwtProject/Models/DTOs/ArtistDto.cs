991717724f65b40cb2b08ae13a6a2b2f692e6b08
Andre <1355686@student.rocvantwente.nl>
2026-01-09 23:42:51 +0100

    Add artist summary endpoint and ArtistDto class
    Added GET api/artists/summary endpoint to return artist summaries (ID, name, total songs) using a new ArtistDto. Introduced ArtistDto class in its own file and also defined it in ArtistsController.cs for compatibility.

---
+++ TemplateJwtProject/Models/DTOs/ArtistDto.cs
@@ -0,0 +1,11 @@
+﻿// Models/ArtistDto.cs
+namespace TemplateJwtProject.Models
+{
+    public class ArtistDto
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; } = null!;
+        public int TotalSongs { get; set; }
+        // Voeg andere velden toe indien nodig
+    }
+}
